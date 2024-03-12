216816bf693252768acbe4fb4a2742b9153b5075
kingston200 <kingjames2009123@gmail.com>
2024-03-12 13:20:31 -0400

    idk

--- src/app.js
+++ src/app.js
@@ -1,4 +1,17 @@
 export function add(a, b) {
-return 2;
-
+  return a + b;
 }
+export function subtract(a, b) {
+  return a - b;
+}
+export function multiply(a, b) {
+  return a * b;
+}
+export function divide(a, b) {
+  return a / b;
+}
+function fizzbuzz(a) {
+  it("fizz buzz returns 1", () => {
+    expect(fizzbuzz(1)).toEqual(1);
+})
+};
--- src/app.test.js
+++ src/app.test.js
@@ -1,9 +1,20 @@
-import {add} from './app'
+import { add, subtract, multiply, divide } from "./app";
 
 describe("my test suite", () => {
-
-    it("first test", () => {
-      expect(add(1,1)).toEqual(2);
-    });
-
+  it("adds 1+1", () => {
+    expect(add(1, 1)).toEqual(2);
+  });
+  it("subtracts 2-1", () => {
+    expect(subtract(2, 1)).toEqual(1);
+  });
+  it("multiplies 5*4", () => {
+    expect(multiply(5, 4)).toEqual(20);
+  });
+  it("divide 2/4", () => {
+    expect(divide(2, 4)).toEqual(0.5);
+const fizz = 3
+  const buzz = 5
+  const fizzbuzz = fizz/buzz
 });
+ 
+ 
