deabfd7728a8f0489bc584fcefa2e55a9b4b1a43
kingston200 <kingjames2009123@gmail.com>
2024-03-26 09:21:20 -0400

    idk

--- src/app.js
+++ src/app.js
@@ -11,5 +11,23 @@ export function divide(a, b) {
   return a / b;
 }
 export function fizzbuzz(a) {
+  if (a % 15 === 0) {
+    return "fizzbuzz"
+  }
+  if (a % 3 === 0) {
+    return "fizz"
+  }
+  if (a % 5 === 0) {
+    return "buzz"
+  }
+
   return a;
+}
+export function fizzbuzzTo(a){
+  if (a % 15 === 0) {
+let Array = Array(0++)
+    .fill(fizzbuzz)
+    .map(() => 0++);
+return 
+  }
 }
--- src/app.test.js
+++ src/app.test.js
@@ -1,20 +1,45 @@
-import { add, subtract, multiply, divide, fizzbuzz } from "./app";
+import { add, subtract, multiply, divide, fizzbuzz, fizzbuzzTo } from "./app";
 
 describe("my test suite", () => {
   it("adds 1+1", () => {
     expect(add(1, 1)).toEqual(2);
   });
-  it("subtracts 2-1", () => {
-    expect(subtract(2, 1)).toEqual(1);
+  it("subtracts 3-1", () => {
+    expect(subtract(3, 1)).toEqual(2);
   });
-  it("multiplies 4*5", () => {
-    expect(multiply(4, 5)).toEqual(20);
+  it("multiplies 5*4", () => {
+    expect(multiply(5, 4)).toEqual(20);
   });
   it("divide 2/4", () => {
     expect(divide(2, 4)).toEqual(0.5);
   });
-  it("passing 1 to fizzbuzz returns 1", () => {
-    expect(fizzbuzz(1)).toEqual(1);
+  it("fizzbuzz returns 1", () => {
+    const result = fizzbuzz(1);
+    expect(result).toEqual(1);
   });
-});
- 
+  it("fizzbuzz returns 2", () => {
+    const result = fizzbuzz(2);
+    expect(result).toEqual(2);
+  });
+  it("passing 3 to fizzbuzz returns fizz", () => {
+    const result = fizzbuzz(3);
+    expect(result).toEqual("fizz");
+  });
+  it("fizzbuzz returns 4", () => {
+    const result = fizzbuzz(4);
+    expect(result).toEqual(4);
+  });
+  it("passing 5 to fizzbuz returns buzz", () => {
+    const result = fizzbuzz(5);
+    expect(result).toEqual("buzz");
+  });
+  it("passing 15 to fizzbuz returns fizzbuzz", () => {
+    const result = fizzbuzz(15);
+    expect(result).toEqual("fizzbuzz");
+  });
+
+  it("refining fizzbuzz with fizzbuzzTo returns fizzbuzz", () => {
+    const result = fizzbuzzTo(15);
+    expect(result).toEqual(Array);
+    });
+  });
