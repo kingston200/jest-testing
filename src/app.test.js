d86791a80c7d8c6dcba9d865f73203e21af8419a
kingston200 <kingjames2009123@gmail.com>
2024-04-16 13:41:30 -0400

    idk

--- src/app.test.js
+++ src/app.test.js
@@ -53,23 +53,7 @@ const result = fizzbuzzTo(1)
       });
       it("fizzbuzzz To list of 15", () => {
         const result = fizzbuzzTo(15);
-        expect(result).toEqual([
-          1,
-          2,
-          "fizz",
-          4,
-          "buzz",
-          "fizz",
-          7,
-          8,
-          "fizz",
-          "buzz",
-          11,
-          "fizz",
-          13,
-          14,
-          "fizzbuzz",
-        ]);
+        expect(result).toEqual([ 1,2, "fizz","buzz","fizz", 7,8,"fizz","buzz", 11,"fizz", 13,14,"fizzbuzz", ]);
       });
     });
 
